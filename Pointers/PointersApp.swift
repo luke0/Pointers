964b04cd919ad295ac84b8d7e45e1c93ec70b528
Luke Inger <luke.inger@jcb-finance.co.uk>
2023-09-04 22:04:13 +0100

    Initial Commit

---
+++ Pointers/PointersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointersApp.swift
+//  Pointers
+//
+//  Created by Luke Inger on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct PointersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
